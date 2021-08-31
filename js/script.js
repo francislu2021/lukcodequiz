81815b7b7c8dbc274f6b06a6ae28c6c57dfc3858
francislu2021 <85589987+francislu2021@users.noreply.github.com>
2021-08-31 14:35:52 -0400

    first run

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Node.js runs on_____?",
+        answer: "Server",
+        options: [
+            "Client",
+            "Server",
+            "Both",
+            "Browser"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Node.js supports which of the following platform?",
+        answer: "All of the above",
+        options: [
+            "Windows",
+            "Mac",
+            "Linux",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Node.js is ____by default",
+        answer: "Asynchronous",
+        options: [
+            "Synchronous",
+            "Multi-threaded",
+            "Asynchronous",
+            "None of above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+
+];
