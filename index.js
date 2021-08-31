511f04c5f0a721b867bb7f792bfde42a371171db
francislu2021 <85589987+francislu2021@users.noreply.github.com>
2021-08-31 15:29:08 -0400

    retouch

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//selecting all required elements
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
@@ -11,7 +10,7 @@ const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 // if startQuiz button clicked
 start_btn.onclick = () => {
-        info_box.classList.add("activeInfo"); //show info box
+        info_box.classList.add("activeInfo"); //here to show info box
     }
     // if exitQuiz button clicked
 exit_btn.onclick = () => {
